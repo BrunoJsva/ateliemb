f693fb2f008037ec55733c0f585c2287d4313950
feodpv02717 <feodpv02717@odontoprev.com.br>
2025-09-30 15:54:28 -0300

    first commit

---
+++ src/test/java/com/sitemarli/ateliemb/AteliembApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitemarli.ateliemb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AteliembApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
