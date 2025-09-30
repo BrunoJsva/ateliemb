f25741508ecdaa0814b7722ad1cf56f62e0a8e1f
Bruno Silva (Github) <bruno.jesusessoaal@gmail.com>
2025-09-30 15:54:28 -0300

    first commit

---
+++ src/main/java/com/sitemarli/ateliemb/AteliembApplication.java
@@ -0,0 +1,13 @@
+package com.sitemarli.ateliemb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AteliembApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AteliembApplication.class, args);
+	}
+
+}
